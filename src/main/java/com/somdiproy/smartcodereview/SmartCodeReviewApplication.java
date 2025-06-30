db6d986065e273b169897d0d6390ffeea2e91cf8
Somdip Roy <somdiproy.roy@gmail.com>
2025-06-30 21:44:03 +0530

    Initial commit

---
+++ src/main/java/com/somdiproy/smartcodereview/SmartCodeReviewApplication.java
@@ -0,0 +1,13 @@
+package com.somdiproy.smartcodereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCodeReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCodeReviewApplication.class, args);
+	}
+
+}
